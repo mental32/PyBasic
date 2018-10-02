52568a4c287f3d43443efa9267a1320e8c460b7b
mental <m3nta1@yahoo.com>
2018-10-02 07:41:21 +0100

    remove comments

--- pybasic_c/_pyb_bytecode.c
+++ pybasic_c/_pyb_bytecode.c
@@ -36,43 +36,15 @@ ByteCodeInterpreter_new(PyTypeObject *type, PyObject *args, PyObject *kwds)
     self = (ByteCodeInterpreter *) type->tp_alloc(type, 0);
 
     if (self != NULL) {
-        // self->first = PyUnicode_FromString("");
-        // if (self->first == NULL) {
-        //     Py_DECREF(self);
-        //     return NULL;
-        // }
-        // self->last = PyUnicode_FromString("");
-        // if (self->last == NULL) {
-        //     Py_DECREF(self);
-        //     return NULL;
-        // }
-        // self->number = 0;
+
     }
+
     return (PyObject *) self;
 }
 
 static int
 ByteCodeInterpreter_init(ByteCodeInterpreter *self, PyObject *args, PyObject *kwds)
 {
-    // static char *kwlist[] = {"source", NULL};
-    // PyObject tmp;
-
-    // if (!PyArg_ParseTuple(args, "s", &self->source)) {
-    //     return -1;
-    // }
-
-    // if (!PyArg_ParseTupleAndKeywords(args, kwds, "|OOi", kwlist,
-    //                                  &first, &last,
-    //                                  &self->number))
-    //     return -1;
-
-    // if (first) {
-    //     tmp = self->first;
-    //     Py_INCREF(first);
-    //     self->first = first;
-    //     Py_XDECREF(tmp);
-    // }
-
     return 0;
 }
 
@@ -86,31 +58,16 @@ ByteCodeInterpreter_run(ByteCodeInterpreter *self, PyObject *args)
         return NULL;
     }
 
-    // Println(source);
-    for (size_t i = 0; i < strlen(source); i++) {
-        printf("%d: %c\n", i, source[i]);
-    }
-
-
-    Py_INCREF(Py_None);
-    return Py_None;
-}
-
-static PyObject *
-ByteCodeInterpreter_run_repl(ByteCodeInterpreter *self)
-{
     Py_INCREF(Py_None);
     return Py_None;
 }
 
 static PyMemberDef ByteCodeInterpreter_members[] = {
-    // {"source", T_STRING, offsetof(ByteCodeInterpreter, source), 0, "source code"},
     {NULL}  /* Sentinel */
 };
 
 static PyMethodDef ByteCodeInterpreter_methods[] = {
     {"run", (PyCFunction) ByteCodeInterpreter_run, METH_VARARGS, "Run the interpreter."},
-    {"run_repl", (PyCFunction) ByteCodeInterpreter_run_repl, METH_NOARGS, "Enter the PyBasic REPL."},
     {NULL}  /* Sentinel */
 };
 
