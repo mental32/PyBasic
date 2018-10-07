70a20dc793079abb199ee04e0f0c952a921b8290
mental <m3nta1@yahoo.com>
2018-10-07 11:43:19 +0100

    remove unused signiture

--- pybasic_c/_pybasic.h
+++ pybasic_c/_pybasic.h
@@ -5,7 +5,6 @@
 /* This section is used when compiling _pybasic.c */
 
 PyTypeObject ByteCodeInterpreterType;
-PyObject *parse_string(PyObject *self, PyObject *args);
 PyObject *IsString(PyObject *self, PyObject *args);
 PyObject *IsInteger(PyObject *self, PyObject *args);
 PyObject *IsConstant(PyObject *self, PyObject *args);
@@ -14,7 +13,6 @@ PyObject *IsConstant(PyObject *self, PyObject *args);
 /* This section is used in modules that use PyBasic's API */
 
 void Println(const char *string);
-PyObject *parse_string(PyObject *self, PyObject *args);
 
 #endif
 #endif
