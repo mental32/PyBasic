57988ed7a983baac70ba6d531ce544541a456813
mental <m3nta1@yahoo.com>
2018-10-11 17:35:53 +0100

    call super with the error format

--- pybasic/errors.py
+++ pybasic/errors.py
@@ -2,7 +2,5 @@ class BadSyntax(Exception):
     def __init__(self, token, source):
         self.__source = source
         self.__token = token
-        super().__init__()
 
-    def __str__(self):
-        return '\n{source}\n{padding}^'.format(source=self.__source, padding=' ' * self.__source.index(self.__token))
+        super().__init__('\n{source}\n{padding}^'.format(source=self.__source, padding=' ' * self.__source.index(self.__token)))
