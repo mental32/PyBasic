589e24d7dad3bd2ceaf1540e9007a04a8fed2af1
mental <m3nta1@yahoo.com>
2018-10-23 00:15:41 +0100

    strip core.py

--- pybasic/core.py
+++ pybasic/core.py
@@ -1,32 +1,9 @@
-import re
-import shlex
+from _pybasic import ByteCodeInterpreter
 
-import _pybasic
+from pybasic import _tokenize
 from .errors import BadSyntax
 
-RE_DIGIT = r'((\d|\d_)\d)'
 
-RE_PRINT = r'(print|PRINT) (\(.*\)|.*)'
-RE_GOTO = r'(goto|GOTO) (\d{1,3})'
-RE_LET = r'(let|LET) ([A-Za-z_]*)( = |=)(.*)'
-RE_IF = r'(if|IF) (.*) (goto|GOTO) (\d{1,3})'
-RE_FOR = r'(for|FOR) (.*) (to|TO) (.*) ((step|STEP) \d{1,9}|)'
-
-patterns = {
-    RE_PRINT: 'print',
-    RE_LET: 'let',
-    RE_IF: 'if',
-}
-
-
-class Interpreter(_pybasic.ByteCodeInterpreter):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._constants = []
-        self._stack = []
-
+class Interpreter(ByteCodeInterpreter):
     def __repr__(self):
         return '<pybasic.Interpreter @ %s>' % hex(id(self)).upper()
-
-    def _tokenize(self, source):
-        return list(iter(shlex.shlex(source).get_token, ''))
