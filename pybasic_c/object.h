f9ad785be1277a7247a2e0070334e83e8d4ce098
mental <m3nta1@yahoo.com>
2018-10-23 00:19:07 +0100

    Add Binding and Label structs

--- pybasic_c/object.h
+++ pybasic_c/object.h
@@ -1,18 +1,39 @@
 #ifndef PYBASIC_OBJECT_H
 #define PYBASIC_OBJECT_H
 
-typedef struct {
-    uint8_t tp;
-    void *ptr;
-} Object;
+#include <stddef.h>
+#include <stdint.h>
 
 enum ObjectType {
     _obj_tp_long,
     _obj_tp_str,
 };
 
+typedef struct {
+    uint8_t tp;
+    void *ptr;
+} Object;
+
+typedef struct {
+    Object *value;
+    PyObject *key;
+} Binding;
+
+typedef struct {
+    long ip;
+    long name;
+} Label;
+
+Binding *newBinding(Object *value, PyObject *key) {
+    Binding *bind = (Binding *) malloc(sizeof(Binding));
+    bind->value = value;
+    bind->key = key;
+
+    return bind;
+}
+
 Object *newObject(uint8_t type, void *ptr) {
-    Object* obj = (Object*) malloc(sizeof(Object));
+    Object *obj = (Object*) malloc(sizeof(Object));
     obj->tp = type;
     obj->ptr = ptr;
 
