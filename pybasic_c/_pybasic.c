d5f9cf482dc58e13c2cf8180154a01533664da0c
mental <m3nta1@yahoo.com>
2018-10-24 00:59:03 +0100

    Expose an instruction dict

--- pybasic_c/_pybasic.c
+++ pybasic_c/_pybasic.c
@@ -3,6 +3,8 @@
 #define PYBASIC_CORE
 #include "_pybasic.h"
 
+PyObject *_ins_dict;
+
 static PyObject *
 set_tokenizer(PyObject *self, PyObject *args)
 {
@@ -13,8 +15,29 @@ set_tokenizer(PyObject *self, PyObject *args)
     Py_RETURN_NONE;
 }
 
+static PyObject *
+get_bytecode_instruction(PyObject *self, PyObject *args)
+{
+    PyObject *key, *item;
+
+    if (!PyArg_ParseTuple(args, "O", &key)) {
+        return NULL;
+    }
+
+    item = PyDict_GetItem(_ins_dict, key);
+
+    if (!item) {
+        PyErr_SetString(PyExc_KeyError, "instruction not found.");
+        return NULL;
+    }
+    
+    Py_INCREF(item);
+    return item;
+}
+
 static PyMethodDef BPyBasicMethods[] = {
     {"set_tokenizer", set_tokenizer, METH_VARARGS, "Set the default tokenizer used."},
+    {"get_bytecode", get_bytecode_instruction, METH_VARARGS, "Get the bytecode for an instruction name."},
     {NULL, NULL, 0, NULL}        /* Sentinel */
 };
 
@@ -38,6 +61,15 @@ PyInit__pybasic(void)
     if (module == NULL) {
         return NULL;
     }
+
+    _ins_dict = PyDict_New();
+    if (!_ins_dict) {
+        Py_XDECREF(_ins_dict);
+        return NULL;
+    } else {
+        Py_INCREF(_ins_dict);
+    }
+    
 
     Py_INCREF(&ByteCodeInterpreterType);
     PyModule_AddObject(module, "ByteCodeInterpreter", (PyObject *) &ByteCodeInterpreterType);
