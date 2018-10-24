68ffe7661f2a112ad29b3752c2f3828dbda2fa42
mental <m3nta1@yahoo.com>
2018-10-24 01:00:18 +0100

    Better run as main interface

--- pybasic/__main__.py
+++ pybasic/__main__.py
@@ -1,12 +1,22 @@
 import sys
+import argparse
 
 if '.' not in sys.path:
 	sys.path.append('.')
 
 from pybasic import Interpreter
 
-if __name__ == '__main__':
+def main(args):
 	_pybvm = Interpreter()
 
-	with open(sys.argv[1]) as inf:
-		_pybvm.run_source(inf.read())
+	if args.file is None:
+		return _pybvm.run_repl()
+
+	with open(args.file) as inf:
+		source = inf.read().strip()
+	_pybvm.run_source(source)
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser()
+	parser.add_argument('file', nargs='?', help='read and run program from file.')
+	main(parser.parse_args())
