f6c01662eaeac13d840142f421cc06cedbe3da04
mental <m3nta1@yahoo.com>
2018-10-26 02:33:36 +0100

    remove unused import

--- pybasic/core.py
+++ pybasic/core.py
@@ -1,7 +1,5 @@
 from _pybasic import ByteCodeInterpreter
 
-from pybasic import _tokenize
-
 class Interpreter(ByteCodeInterpreter):
     def __repr__(self):
         return '<pybasic.Interpreter @ %s>' % hex(id(self)).upper()
