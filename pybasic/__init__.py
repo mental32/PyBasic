3826fa8e5af5eb2598f833aa204da46071e2097d
mental <m3nta1@yahoo.com>
2018-10-26 02:36:13 +0100

    prepare to use parser.tokenize

--- pybasic/__init__.py
+++ pybasic/__init__.py
@@ -2,13 +2,10 @@ import shlex
 
 import _pybasic
 
-def _tokenize(source):
-    return [list(iter(shlex.shlex(line).get_token, '')) for line in source.split('\n')]
-
-_pybasic.set_tokenizer(_tokenize)
-
 from . import parser
 from .core import Interpreter
+
+_pybasic.set_tokenizer(parser.tokenize)
 
 __version__ = '0.1.0'
 __author__ = 'mental'
