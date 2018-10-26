40ee2a40fe08ba675f6a756c3162cecf7524d0fb
mental <m3nta1@yahoo.com>
2018-10-27 00:31:00 +0100

    Add syntax and token stream parser

--- pybasic/parser.py
+++ pybasic/parser.py
@@ -1,1 +1,213 @@
-from pybasic import _tokenize
+import shlex
+import struct
+import pprint
+
+import _pybasic
+
+from pybasic import _bvm_ins
+
+is_string = lambda token: (len(token) > 2 and (token[0], token[-1]) == ('"', '"'))
+
+_reserved = [
+    'PRINT',
+    'LET',
+    'GOTO',
+    'END',
+    'IF'
+]
+
+class Operator:
+    __bvm_ops = {
+        '+': _bvm_ins['binary_add'],
+        '-': _bvm_ins['binary_sub'],
+        '*': _bvm_ins['binary_mul'],
+        '/': _bvm_ins['binary_div'],
+    }
+
+    def __init__(self, data):
+        self.data = data
+
+    @property
+    def as_bvm_ins(self):
+        return self.__bvm_ops[self.data]
+
+
+class Logic(Operator):
+    pass
+
+
+def _get_concrete(token):
+    ttype = _pybasic.type(token)
+
+    if ttype == 'I':
+        return int(token)
+
+    elif token in ('!=', '=='):
+        return Logic(token)
+
+    elif token in '+-*/':
+        return Operator(token)
+
+    elif token in ('!', '=', 'GOTO'):
+        return None
+
+    return token
+
+def evaluate(constants, tokens):
+    expr = bytearray()
+
+    if not tokens:
+        return expr
+
+    types = {_pybasic.type(t) for t in tokens}
+
+    if 'V' not in types:
+        # Expression appears to be static (containing no refrences)
+
+        # "Using eval is cheating!" 
+        # I'm using eval because I can and I know it's absolutely safe!
+        # Ergo to not use eval would be the silly answer
+        rv = eval(''.join(tokens))
+    else:
+        stream = iter(reversed(tokens))
+        rv = _get_concrete(next(stream))
+
+    while True:
+        if isinstance(rv, int):
+            if 255 > rv >= 0:
+                expr.append(_bvm_ins['load_byte'])
+                expr.extend(struct.pack('=B', rv))
+            else:
+                expr.append(_bvm_ins['load_long'])
+                expr.extend(struct.pack('=l', rv))
+
+        elif isinstance(rv, bool):
+            expr.append(_bvm_ins['load_byte'])
+            expr.extend(struct.pack('=?', rv))
+
+        elif isinstance(rv, Logic):
+            expr.extend(evaluate(constants, tuple(stream)))
+
+            if rv.data[0] == '!':
+                expr.append(_bvm_ins['cmp'])
+                expr.append(_bvm_ins['not'])
+            else:
+                expr.append(_bvm_ins['cmp'])
+
+        elif isinstance(rv, Operator):
+            expr.extend(evaluate(constants, tuple(stream)))
+            expr.append(rv.as_bvm_ins)
+
+        elif isinstance(rv, str):
+            expr.append(_bvm_ins['load_const'])
+
+            if rv not in constants:
+                constants.append(rv)
+
+            expr.append(constants.index(rv))
+
+        if 'V' not in types:
+            break
+
+        try:
+            rv = _get_concrete(next(stream))
+        except StopIteration:
+            break
+
+    return expr
+
+def tokenize(source):
+    _bytecode = bytearray()
+    parsed = {}
+    labels = {}
+    refs = {}
+    constants = []
+
+    # While creating the parsed tokens
+    # Extract metadata and perform analysis
+    # On the source.
+    for line in source.strip().split('\n'):
+        rparity = 0
+        row = []
+
+        if '!=' in line:
+            rparity |= 1
+
+        it = iter(shlex.shlex(line.strip()).get_token, '')
+        ln = int(next(it))
+        lt = None
+
+        for token in it:
+            if token not in _reserved and token not in constants and (token.isalpha() or is_string(token)):
+                constants.append(token)
+
+            elif token == '=' and lt == '!' and rparity & 1:
+                row.pop()
+                row.append('!=')
+                lt = token
+                continue
+
+            row.append(token)
+            lt = token
+
+        if 'GOTO' in row:
+            refs[ln] = int(row[row.index('GOTO') + 1])
+
+        parsed[ln] = row
+
+    _bytecode.extend(bytes(':'.join(constants), encoding='utf8'))
+    _bytecode.append(_bvm_ins['data_end'])
+
+    _bytecode.append(_bvm_ins['return'])
+
+    parsed = {k: parsed[k] for k in sorted(parsed)}
+
+    for ln, src in parsed.items():
+        labels[ln] = [len(_bytecode)]
+
+        if src[0] == 'LET':
+            _bytecode.extend(evaluate(constants, src[3:]))
+
+            _bytecode.append(_bvm_ins['load_const'])
+            _bytecode.append(constants.index(src[1]))
+
+            _bytecode.append(_bvm_ins['store'])
+
+        elif src[0] == 'IF':
+            *e, _g_ln = src[1:]
+
+            _bytecode.extend(evaluate(constants, e[:-1]))
+
+            _bytecode.append(_bvm_ins['pop_jmp_true'])
+            _bytecode.extend(struct.pack('=h', labels[int(_g_ln)][0] - labels[ln][0]))
+
+        elif src[0] == 'END':
+            _bytecode.append(_bvm_ins['return'])
+
+        elif src[0] == 'PRINT':
+            _bytecode.extend(evaluate(constants, src[1:]))
+            _bytecode.append(_bvm_ins['print'])
+
+        elif len(src) >= 3 and src[1] == '=' and src[2] != '=':
+            # re assignment
+            _bytecode.extend(evaluate(constants, src[1:]))
+
+            _bytecode.append(_bvm_ins['load_const'])
+            _bytecode.append(constants.index(src[0]))
+
+            _bytecode.append(_bvm_ins['store'])
+        else:
+            _bytecode.extend(evaluate(constants, src))
+
+        labels[ln].append(len(_bytecode) - labels[ln][0])
+
+    # pprint.pprint((parsed, constants))
+
+    # litecode = list(_bytecode)
+    # for ln, data in labels.items():
+    #     start, size = data
+    #     print(ln, litecode[start:start + size])
+
+    # pprint.pprint(_bytecode)
+
+    return _bytecode
