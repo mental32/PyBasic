fb95e11cf57e38eec675e2765a622ed40c69b16d
mental <m3nta1@yahoo.com>
2018-10-30 15:51:21 +0000

    Explicit cast of data base pointer and NULLify pointer after data read

--- pybasic_c/bvm/vm.c
+++ pybasic_c/bvm/vm.c
@@ -120,13 +120,14 @@ int BytecodeVirtualMachine_main(uint8_t *bytecode, size_t bytecode_size) {
 
     // Read size of constants pool.
     short dp = 0, data_size = *((short*) (vm->ip));
-    char *p = vm->ip + 2, *end = (char *)(data_size + vm->ip + 2);
+    char *p = (char *)vm->ip + 2, *end = (char *)(data_size + vm->ip + 2);
 
     while (p < end) {
         vm->data[dp++] = p;
         p += strlen(p) + 1;
     }
 
+    p = NULL;
     // Then jump ahead of it.
     vm->ip += data_size;
 
