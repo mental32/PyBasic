633d707d07ea7d08f4b5e482258469cad9c352de
mental <m3nta1@yahoo.com>
2018-11-20 02:22:52 +0000

    impl Object_(R|r)ebase

--- pybasic_c/bvm/lib/object/base.c
+++ pybasic_c/bvm/lib/object/base.c
@@ -20,26 +20,36 @@ Destructor for an object.
 */
 int Object_Free(Object *obj)
 {
-	if (!obj) {
-		return 0;
-	}
+    if (!obj) {
+        return 0;
+    }
 
-	free(obj);
-	return 1;
+    free(obj);
+    return 1;
 }
 
 /*
 Mirror the contents of one object onto another (Effectively a memcpy).
+NOTE: This will discard the src->ptr, DO NOT LOSE THE src->ptr UNLESS YOU LIKE MEMORY LEAKS.
 */
-Object *Object_Rebase(Object *src, Object *dst)
+Object *Object_Rebase(Object *dst, Object *src)
 {
+    dst->tp = src->tp;
+    dst->rc = src->rc;
+    dst->ptr = src->ptr;
 
+    return dst;
 }
 
 /*
-Modify the contents of one object with that properties of an abstract Object.
+Modify the contents of one object with that properties of an abstract Object.#
+NOTE: This will discard the src->ptr, DO NOT LOSE THE src->ptr UNLESS YOU LIKE MEMORY LEAKS.
 */
-Object *Object_rebase(uint8_t tp, void *data, uint8_t rc)
+Object *Object_rebase(Object *dst, uint8_t tp, void *ptr, uint8_t rc)
 {
-	
+    dst->tp = tp;
+    dst->rc = rc;
+    dst->ptr = ptr;
+
+    return dst;
 }
