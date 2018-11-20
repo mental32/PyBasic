2ce0476f96862270b5ed175a7ba2f34e1bd83fa5
mental <m3nta1@yahoo.com>
2018-11-20 02:17:45 +0000

    impl constructors for String and Integer

--- pybasic_c/bvm/lib/object/integer.c
+++ pybasic_c/bvm/lib/object/integer.c
@@ -1,8 +1,9 @@
 #include "base.h"
 
 /*
+Constructor for an integer.
 */
 Object *Object_Integer(long n)
 {
-
+    return Object_New(generic_int, (void *) n);
 }
--- pybasic_c/bvm/lib/object/string.c
+++ pybasic_c/bvm/lib/object/string.c
@@ -5,5 +5,5 @@ Constructor for a string Object.
 */
 Object *Object_String(char *str)
 {
-
+    return Object_New(generic_str, str);
 }
