0b333c75dd12a61f1db0471ec486a2843e9b997d
mental <m3nta1@yahoo.com>
2018-11-21 02:16:10 +0000

    move inline VMState stack functions

--- pybasic_c/bvm/vm/vm.h
+++ pybasic_c/bvm/vm/vm.h
@@ -6,56 +6,34 @@
 #include "../lib/status.h"
 #include "../lib/object/base.h"
 
+/*
+Helper struct for containing bytecode metadata.
+*/
+typedef struct {
+    uint16_t varspace_size;
+} BytecodeHeader;
+
+/*
+implementation of a VirtualMachine state.
+*/
 typedef struct {
     uint8_t *ip;
     uint8_t _running;
     uint8_t __state;
 
-    char **data;
+    char **const_pool;
 
     size_t insc;
     size_t sp;
+
+    BytecodeHeader *header;
 
     Object **stack;
     Object **varspace;
 } VMState;
 
-typedef struct {
-
-} ByteCodeHeader;
-
 VMState *VirtualMachine_new(uint8_t *bytecode);
 int VirtualMachine_init(VMState *vm);
 int VirtualMachine_free(VMState *vm);
-
-inline int VMState_pushstack(VMState *vm, Object *item) {
-    if (!item || !vm) {
-        return 0;
-    } else {
-        vm->stack[vm->sp++] = item;
-    }
-
-    return 1;
-}
-
-inline Object *VMState_popstack(VMState *vm) {
-    if (!vm) {
-        return NULL;
-    }
-
-    Object *item = vm->stack[--vm->sp];
-    vm->stack[vm->sp] = NULL;
-    return item;
-}
-
-inline Object *VMState_resolve_ref(VMState *vm, Object *ref) {
-    Object *obj = ref;
-
-    while (obj->tp == generic_ref) {
-        obj = vm->varspace[*((uint8_t*)obj->ptr)];
-    }
-
-    return obj;
-}
 
 #endif
