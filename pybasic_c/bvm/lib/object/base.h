a9903f311d6cce8158fc7d44bced2fe2c9c3eb34
mental <m3nta1@yahoo.com>
2018-11-21 02:15:40 +0000

    Add Object XINCREF, XDECREF, FDECREF, UDECREF

--- pybasic_c/bvm/lib/object/base.h
+++ pybasic_c/bvm/lib/object/base.h
@@ -15,6 +15,22 @@ int Object_Free(Object *obj);
 Object *Object_Rebase(Object *src, Object *dst);
 Object *Object_rebase(Object *dst, uint8_t tp, void *data, uint8_t rc);
 
+/* Increase an objects refrence count (without any checks) */
 #define Object_INCREF(o) o->rc++
+
+/* If the object is not null, increase its refrence count. */
+#define Object_XINCREF(o) o->rc++
+
+/* Decrease an objects refrence count (without any checks) */
 #define Object_DECREF(o) o->rc--
+
+/* If the object is not null, decrease its refrence count. */
+#define Object_XDECREF(o) if (o != NULL) o->rc--
+
+/* If decreasing the objects refrence count will cause it to be forgotten, it is free'd. */
+#define Object_FDECREF(o) if (!--o->rc) Object_free(o)
+
+/* Chains Object_XDECREF (null check) and Object_FDECREF (free if forgotten). */
+#define Object_UDECREF(o) if (o != NULL && !(--o->rc)) Object_Free(o)
+
 #endif 
