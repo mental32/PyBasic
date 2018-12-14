b8fe72452a20dc88afc86403539017a486ad934f
mental <m3nta1@yahoo.com>
2018-12-14 10:57:42 +0000

    Move BytecodeVirtualMachine_main definintion

--- pybasic_c/bvm/basic.h
+++ pybasic_c/bvm/basic.h
@@ -1,63 +1,7 @@
 #ifndef _BASIC_BVM_H
 #define _BASIC_BVM_H
 
-#include "vm/vm.h"
-
 char *BytecodeVirtualMachine_err;
-
-/*
-run the entire bytecode to completion.
-NOTE:This is a blocking operation and may not give up control.
-
-Parameters
-----------
- uint8_t *bytecode - A pointer to the base of the bytecode that the VM should read.
- size_t bytecode_size - The size of the bytecode.
-*/
-int BytecodeVirtualMachine_main(uint8_t *bytecode, size_t bytecode_size)
-{
-    if (bytecode_size < 2) {
-        return OK;
-    }
-
-    #if __BVM_DEBUG
-    printf("! Creating VMState instance.\n");
-    #endif
-
-    VMState *vm = VirtualMachine_new(bytecode);
-
-    #if __BVM_DEBUG
-    printf("! Initializing VMState...");
-    #endif
-
-    if (!VirtualMachine_init(vm)) {
-        return PANIC;
-    }
-
-    #if __BVM_DEBUG
-    printf("OK\n! Entering VMState loop\n");
-    #endif
-
-    vm->_running = 1;
-
-    while (vm->_running) {
-        vm->ip++;
-        vm->insc++;
-
-        #if __BVM_DEBUG
-        printf("!-ins=%d :: ip=%p :: sp=%ld\n", *vm->ip, vm->ip, vm->sp);
-        #endif
-
-        BytecodeVirtualMachine_step(vm);
-    }
-
-    if (vm->_running)
-    {
-        vm->err = "VM exited while running.\n";
-        vm->__state = PANIC;
-    }
-
-    return VirtualMachine_free(vm);
-}
+int BytecodeVirtualMachine_main(uint8_t *bytecode, size_t bytecode_size);
 
 #endif
